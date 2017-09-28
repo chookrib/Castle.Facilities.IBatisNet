dda33a54bee4436f9ffd8346b9640e2b255d0b7c
chookrib <chookrib@gmail.com>
2017-09-28 11:23:52 +0800

    modify build scripts

--- src/build-msbuild15.bat
+++ src/build-msbuild15.bat
@@ -1,8 +1,5 @@
-rem ***********************************************************************************************
-rem Download BuildTools from https://www.visualstudio.com/downloads/ and install first.
-rem ***********************************************************************************************
-rem x86 build command
-rem "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" .\Castle.Facilities.IBatisNet.sln /t:Rebuild /p:Configuration=Release /p:WarningLevel=0
-rem x64 build command
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\amd64\MSBuild.exe" .\Castle.Facilities.IBatisNet.sln /t:Rebuild /p:Configuration=Release /p:WarningLevel=0
+@echo If not install Visual Studio 2017, download BuildTools from https://www.visualstudio.com/downloads/ and install first.
+nuget-4.3.0.4406.exe restore
+SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\;C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin;C:\Program Files (x86)\Microsoft Visual Studio\2017\Visual Studio Community\MSBuild\15.0\Bin;
+MSBuild.exe .\Castle.Facilities.IBatisNet.sln /t:Rebuild /p:Configuration=Release /p:WarningLevel=0
 pause
